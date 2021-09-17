6cb4d4c0e94842419ee9dc67ffaca295e17d4c9d
Daniel <daniel.fb.jr@gmail.com>
2021-09-17 20:38:30 -0300

    Aula

---
+++ Aula2/Aula2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Botão clicado!");
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Volte sempre");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Teste 1");
+        }
+    }
+}
---
+++ Aula4/Aula4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            short valor = 10;
+            int idade = 20;
+
+            double velocidade = 32768;
+
+            valor = Convert.ToInt16(idade);
+            //idade = Convert.ToInt32(velocidade);
+            //velocidade = idade;
+
+            valor = Convert.ToInt16(velocidade);
+
+            Console.WriteLine("Valor = " + valor);
+            Console.WriteLine("Idade = " + idade);
+            Console.WriteLine("Velocidade = " + velocidade);
+
+
+            /*
+            
+            +
+            -
+            *
+            /
+            %
+            
+
+            A comparar com B
+            B com C
+            A com 0
+            B com (C + 5 + B)
+
+            >
+            >=
+            <
+            <=
+            ==
+            !=
+
+            &&
+            ||
+            ^
+
+            AV1 = 5
+            av1 < 5 ??
+
+
+
+
+             */
+
+
+             */
+        }
+    }
+}
